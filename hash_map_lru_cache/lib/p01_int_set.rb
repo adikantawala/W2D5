425fb6cdc6d504c2a34b6768bf45a2b90ec40047
adikantawala <adikan@ucla.edu>
2018-11-02 14:20:24 -0700

    Finish p01_int_set.rb

--- hash_map_lru_cache/lib/p01_int_set.rb
+++ hash_map_lru_cache/lib/p01_int_set.rb
@@ -1,19 +1,34 @@
+require 'byebug'
+
 class MaxIntSet
   def initialize(max)
+    @store = Array.new(max, false)
   end
 
   def insert(num)
+    @store[num] = true if is_valid?(num)
   end
 
   def remove(num)
+    @store[num] = false
   end
 
   def include?(num)
+    if @store[num] == true
+      return true
+    else
+      return false
+    end
   end
 
   private
 
   def is_valid?(num)
+    if num > @store.length || num < 0
+      raise "Out of bounds"
+    else
+      return true
+    end
   end
 
   def validate!(num)
@@ -27,18 +42,22 @@ class IntSet
   end
 
   def insert(num)
+    # byebug
+    self[num] << num
   end
 
   def remove(num)
+    self[num].delete(num)
   end
 
   def include?(num)
+    self[num].include?(num)
   end
 
   private
 
   def [](num)
-    # optional but useful; return the bucket corresponding to `num`
+    @store[num % num_buckets]
   end
 
   def num_buckets
@@ -55,24 +74,50 @@ class ResizingIntSet
   end
 
   def insert(num)
+    @count +=1
+    if @count % num_buckets == 1
+      self.resize!
+    end
+    # byebug
+    if self.include?(num)
+      self.remove(num)
+    end
+    self[num]<< num unless self.include?(num)
   end
 
   def remove(num)
+    if self[num].include?(num)
+      self[num].delete(num)
+      @count -= 1
+    end
   end
 
   def include?(num)
+    self[num].include?(num)
   end
 
-  private
+  # private
 
   def [](num)
-    # optional but useful; return the bucket corresponding to `num`
+    @store[num % num_buckets]
   end
+  # def []=(num, val)
+  #   @store[num % num_buckets] << val
+  # end
 
   def num_buckets
     @store.length
   end
 
   def resize!
+    @store += Array.new(@store.length) { Array.new }
+    (@store.length / 2).times do |bucket|
+      @store[bucket].each do |el|
+        @store[bucket].delete(el)
+        insert(el)
+      end
+    end
+    # @count = 0
   end
+
 end
